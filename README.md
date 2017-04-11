a0b10083c84bb52d8b0c69fe58ae6cce799dc17a
MrSakamoto-san <me@felis.cat>
2017-04-11 07:53:02 -0400

    real initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Relevant urban
+This module was meant to replace ~~shitty~~ [urban](https://github.com/mvrilo/urban) module
+as it uses callbacks and does useless stuff.
+
+```js
+const urban = require('relevant-urban');
+```
+
+| Methods                |           returns               |                         description                       |
+|------------------------|---------------------------------|-----------------------------------------------------------|
+| urban('string')        | `=> Promise<Definition>`        | Gets first definition from urban matching provided query  |
+| urban.random()         | `=> Promise<Definition>`        | Gets random definition from urban                         |
+| urban.all('string')    | `=> Promise<Array<Definition>>` | Gets all definitions from page 1 matching provided query  |
+| urban.random('string') | `=> Promise<Definition>`        | Gets random definition from urban matching provided query |
+| urban.search('string'[, page]) | `=> Promise<api response>` | Gets raw response from api without any formating       |
+
+## Example output
+*NOTE: `urban.random()` method doesn't have tags and sounds*
+
+```js
+  Definition {
+  id: 69266,
+  word: 'hello',
+  definition: 'what you say when your talking casually with friends and your mom walks in the room',
+  example: 'What the hell(mom enters)-o mom.',
+  urbanURL: 'http://hello.urbanup.com/69266',
+  author: 'mad at the world',
+  thumbsUp: 3297,
+  thumbsDown: 936,
+  tags: [ 'hi', 'hey', 'greeting', 'yo', 'goodbye' ],
+  sounds: [ 'http://media.urbandictionary.com/sound/hello-7503.mp3',
+  'http://media.urbandictionary.com/sound/hello-9778.mp3',
+  'http://media.urbandictionary.com/sound/hello-9897.mp3',
+  'http://media.urbandictionary.com/sound/hello-10454.mp3',
+  ...]
+  }
+```
---
+++ urban.js
@@ -0,0 +1,108 @@
+const snekfetch = require('snekfetch');
+const api = 'https://api.urbandictionary.com/v0';
+const ArrayRandom = array => array[Math.floor(Math.random() * array.length)];
+
+function search(query, page = 1) {
+  return new snekfetch('GET', `${api}/define?page=${page}&term=${query}`)
+    .then(res => res.body)
+    .then(body => body.result_type == 'no_results' ? null : body);
+}
+
+function first(query) {
+  return search(query)
+    .then(body => body ?
+      Object.assign(new Definition(body.list[0]), {
+        tags: Array.from(new Set(body.tags)),
+        sounds: body.sounds
+      }) : null);
+}
+
+function all(query) {
+  return search(query)
+    .then(body => body ?
+      Object.assign(body.list.map(d => new Definition(d)), {
+        tags: Array.from(new Set(body.tags)),
+        sounds: body.sounds
+      }) : null);
+}
+
+function random(query = null) {
+  if (!query) return new snekfetch('GET', `${api}/random`)
+    .then(res => res.body)
+    .then(body => body.result_type == 'no_results' ? null : new Definition(body.list[0]));
+  else return search(query)
+    .then(body => body ?
+      Object.assign(new Definition(ArrayRandom(body.list)), {
+        tags: Array.from(new Set(body.tags)),
+        sounds: body.sounds
+      }) : null);
+}
+
+class Definition {
+  constructor({
+    defid,
+    word,
+    definition,
+    example,
+    permalink,
+    thumbs_up,
+    author,
+    thumbs_down
+  }) {
+
+    /**
+     * ID of definition.
+     * @type {number}
+     */
+    this.id = defid;
+
+    /**
+     * Word used for definition.
+     * @type {string}
+     */
+    this.word = word;
+
+    /**
+     * Definition itself.
+     * @type {string}
+     */
+    this.definition = definition;
+
+    /**
+     * Definition example.
+     * @type {string}
+     */
+    this.example = example;
+
+    /**
+     * Definition URL.
+     * @type {string}
+     */
+    this.urbanURL = permalink;
+
+    /**
+     * Definition author name.
+     * @type {string}
+     */
+    this.author = author;
+
+    /**
+     * Definition thumbs up.
+     * @type {number}
+     */
+    this.thumbsUp = thumbs_up;
+
+    /**
+     * Definition thumbs down.
+     * @type {number}
+     */
+    this.thumbsDown = thumbs_down;
+  }
+}
+
+module.exports = Object.assign(first, {
+  all,
+  random,
+  search,
+  version: '1.0.0'
+});
