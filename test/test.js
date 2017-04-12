7c3445799c0763c4e6a519d4284ca2cd2b1287f9
MrSakamoto-san <me@felis.cat>
2017-04-12 01:28:25 -0400

    litle changes

--- test/test.js
+++ test/test.js
@@ -2,21 +2,21 @@ const urban = require('../urban.js');
 const test = require('ava');
 
 test('Fetching random urban definition', t => urban.random()
-.then(d =>t.truthy(d))
-.catch(e => t.falsy(e)));
+    .then(d => t.truthy(d))
+    .catch(e => t.falsy(e)));
 
 test('Fetching \'hello\' urban definition', t => urban('hello')
-.then(d =>t.truthy(d))
-.catch(e => t.falsy(e)));
+    .then(d => t.truthy(d))
+    .catch(e => t.falsy(e)));
 
 test('Fetching \'fsiojgjsgjsgihsghghjsh\' urban definition (success if not found)', t => urban('fsiojgjsgjsgihsghghjsh')
-.then(d =>t.falsy(d))
-.catch(e => t.falsy(e)));
+    .then(d => t.falsy(d))
+    .catch(e => t.falsy(e)));
 
 test('Fetching random specific urban definition', t => urban.random('hello')
-.then(d =>t.truthy(d))
-.catch(e => t.falsy(e)));
+    .then(d => t.truthy(d))
+    .catch(e => t.falsy(e)));
 
 test('Fetching all specific urban definitions', t => urban.all('hello')
-.then(d =>t.true(typeof d == 'object'))
-.catch(e => t.falsy(e)));
+    .then(d => t.true(typeof d == 'object'))
+    .catch(e => t.falsy(e)));
