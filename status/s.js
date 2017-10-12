948ce9e18b6263099ef5cf774488686c31ba6683
black-c4t <black-c4t@protonmail.com>
2017-10-12 16:56:16 +0300

    not today hungry eyes

--- status/s.js
+++ status/s.js
@@ -1,16 +1,15 @@
 const Status = require('./Status');
-const { CI, TRAVIS, TRAVIS_REPO_SLUG, TRAVIS_COMMIT } = process.env;
+const { CI, TRAVIS, TRAVIS_REPO_SLUG, TRAVIS_COMMIT, STATUS_TOKEN } = process.env;
 const [state, label] = process.argv.slice(2);
 
 if (!CI || !TRAVIS) return null;
-console.log(TRAVIS_COMMIT);
 
-// const status = new Status({
-//   label,
-//   sha: head_commit.id,
-//   repo: TRAVIS_REPO_SLUG,
-//   token: '8aecb733f5897c3c626fe33d8f9bc075698b2300',
-// });
-//
-// if (state == 'pending') return status.start();
+const status = new Status({
+  label,
+  sha: TRAVIS_COMMIT,
+  token: STATUS_TOKEN,
+  repo: TRAVIS_REPO_SLUG,
+});
+
+if (state == 'pending') return status.start();
 return null;
